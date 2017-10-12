b41832558c41e033ea7d8a30b3116a66150c359c
Sam Waller <samuellwaller@gmail.com>
2017-10-12 01:29:49 -0400

    basic but ugly ugly functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,22 @@
 // Check off todos by clicking
-$('li').click(function(){
+$('ul').on("click", "li", function(){
 	$(this).toggleClass("completed")
+})
+
+//click on X to delete todo
+$('ul').on('click', 'span', function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove()
+	})
+	event.stopPropagation()
+})
+
+$('input').keypress(function(event){
+	if(event.which === 13){
+		//grab new todo text from input
+		var todoText = $(this).val()
+		$(this).val("")
+		//create new li and add to ul
+		$('ul').append('<li><span>X </span>' + todoText + '</li>')
+	}
 })
