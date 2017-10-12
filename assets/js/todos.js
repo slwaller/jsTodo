48801083d1476e86f33c78463cfeff5ed20d47df
Sam Waller <samuellwaller@gmail.com>
2017-10-12 02:37:26 -0400

    completo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,10 @@ $('input').keypress(function(event){
 		var todoText = $(this).val()
 		$(this).val("")
 		//create new li and add to ul
-		$('ul').append('<li><span>X </span>' + todoText + '</li>')
+		$('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>')
 	}
+})
+
+$('.fa-plus').click(function(){
+	$('input').fadeToggle()
 })
